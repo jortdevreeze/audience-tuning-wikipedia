0b27a531bfa744214d012042b06d314130f74409
Jort de Vreeze <31477876+jortdevreeze@users.noreply.github.com>
2019-03-07 12:14:15 +0100

    Update images.py

--- config/images.py
+++ config/images.py
@@ -6,8 +6,8 @@ Created on Mon Mar  4 11:58:55 2019
 """
 
 # Define paths and filenames for importing and exporting data
-pathName = 'C:/Users/jdevreeze/Desktop/Scheduled/Python'
-fileName = 'data/Dataset Study 1 (Edits).csv'
+pathName = ''
+fileName = 'data/dataset.csv'
 exportPath = 'images'
 
 # Define variable names which are used in the dataset
