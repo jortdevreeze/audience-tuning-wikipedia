82186e64f8fe78360bc9006adc88716d06cbb9c3
Jort de Vreeze <31477876+jortdevreeze@users.noreply.github.com>
2019-03-07 12:34:47 +0100

    Add files via upload

---
+++ config/common.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 12:29:56 2019
+
+@author: jdevreeze
+"""
+
+# Define paths and filenames for importing and exporting data
+pathName = 'C:/Users/jdevreeze/Desktop/Scheduled/Python'
+fileName = 'data/Dataset Study 1 (Edits).csv'
+
+# Define variable names which are used in the dataset
+previousEdit = 'PreviousEdit'
+currentEdit = 'CurrentEdit'
+languageCode = 'Language'
+parentTitle = 'ParentTitle'
+editId = 'EditId'
