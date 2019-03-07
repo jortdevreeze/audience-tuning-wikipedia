56829f3b389592abcb3ccf485570014f91cdbde4
Jort de Vreeze <31477876+jortdevreeze@users.noreply.github.com>
2019-03-07 12:34:15 +0100

    Update images.py

--- config/images.py
+++ config/images.py
@@ -5,17 +5,8 @@ Created on Mon Mar  4 11:58:55 2019
 @author: jdevreeze
 """
 
-# Define paths and filenames for importing and exporting data
-pathName = ''
-fileName = 'data/dataset.csv'
+# Define path for exporting data
 exportPath = 'images'
-
-# Define variable names which are used in the dataset
-previousEdit = 'PreviousEdit'
-currentEdit = 'CurrentEdit'
-languageCode = 'Language'
-parentTitle = 'ParentTitle'
-editId = 'EditId'
 
 # Define text variables
 fontSize = 16
