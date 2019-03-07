c30acdcc4f95e83aa30254d9c4c5a8804d5ec89f
Jort de Vreeze <31477876+jortdevreeze@users.noreply.github.com>
2019-03-07 12:35:13 +0100

    Update common.py

--- config/common.py
+++ config/common.py
@@ -6,8 +6,8 @@ Created on Thu Mar  7 12:29:56 2019
 """
 
 # Define paths and filenames for importing and exporting data
-pathName = 'C:/Users/jdevreeze/Desktop/Scheduled/Python'
-fileName = 'data/Dataset Study 1 (Edits).csv'
+pathName = ''
+fileName = 'data/dataset.csv'
 
 # Define variable names which are used in the dataset
 previousEdit = 'PreviousEdit'
